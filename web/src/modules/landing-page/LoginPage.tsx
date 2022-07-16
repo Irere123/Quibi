5eb166ba2d794c6063c3b1f259849324efadf8a9
irere123 <irere2020@outlook.com>
2022-07-16 18:11:39 +0200

    update: changed the login page

--- web/src/modules/landing-page/LoginPage.tsx
+++ web/src/modules/landing-page/LoginPage.tsx
@@ -12,7 +12,7 @@ interface LoginButtonProps {
 const LoginButton: React.FC<LoginButtonProps> = ({ oauthUrl, children }) => {
   return (
     <button
-      className="flex items-center gap-3 border-2 border-black rounded justify-center text-base py-2 mt-2"
+      className="flex items-center gap-3 bg-primary-700 hover:bg-primary-600 rounded justify-center text-primary-200 py-2 mt-2"
       onClick={() => {
         window.location.href = `${oauthUrl}`;
       }}
@@ -34,19 +34,22 @@ export const LoginPage: React.FC = () => {
       <HeaderController embed={{}} title="Login" />
       <div className="hidden sm:flex" />
       <div className="flex justify-self-center self-center sm:hidden">
-        <h3>Quibi</h3>
+        <h3 className="text-secondary-300 text-5xl">Quibi</h3>
       </div>
       <div className="flex justify-center items-center w-full">
-        <div className="landingPage__loginCard bg-primary-300 py-7 px-6 shadow-lg">
+        <div className="landingPage__loginCard bg-secondary-300 py-7 px-6 shadow-lg">
           <div className="flex gap-2 flex-col">
-            <span className="text-3xl font-bold">Welcome</span>
-            <div className="flex-wrap">
+            <span className="text-3xl font-bold text-primary-100">Welcome</span>
+            <div className="flex-wrap text-primary-200">
               By logging in you accept our&nbsp;
-              <a href="/privacy-policy.html" className=" hover:underline">
+              <a
+                href="/privacy-policy.html"
+                className="text-accent hover:underline"
+              >
                 Privacy Policy
               </a>
               &nbsp;and&nbsp;
-              <a href="/terms.html" className="hover:underline">
+              <a href="/terms.html" className="text-accent hover:underline">
                 Terms of Service
               </a>
               .
@@ -70,9 +73,9 @@ export const LoginPage: React.FC = () => {
       </div>
       <div className="flex flex-row absolute bottom-0 w-full justify-between px-5 py-5 mt-auto items-center sm:px-7">
         <div className="hidden sm:flex">
-          <h3>Quibi</h3>
+          <h3 className="text-secondary-300">Quibi</h3>
         </div>
-        <div className="flex flex-row gap-3">
+        <div className="flex flex-row gap-3 text-primary-100">
           <p>English</p>
           <a href="/developer" className="ml-2">
             {t("footer.link_1")}
