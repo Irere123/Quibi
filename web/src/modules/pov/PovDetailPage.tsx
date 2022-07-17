86ecab247611263835636c18f1295f0ba93bcafb
irere123 <irere2020@outlook.com>
2022-07-17 17:36:22 +0200

    refactor: deleted the povheader component

--- web/src/modules/pov/PovDetailPage.tsx
+++ web/src/modules/pov/PovDetailPage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { PovHeader } from "../../ui/PovHeader";
+import { PlusIcon } from "../../icons";
+import { BoxedIcon } from "../../ui/BoxedIcon";
+import { HeaderController } from "../components/HeaderController";
 import { LeftPannel } from "../dashboard/Panels";
 import { MiddlePanel } from "../layouts/GridPanels";
 import { MainLayout } from "../layouts/MainLayout";
@@ -9,9 +11,19 @@ export const PovDetailPage: React.FC = () => {
   return (
     <MainLayout leftPanel={<LeftPannel />} rightPanel={<CommentsPanel />}>
       <MiddlePanel
-        stickyChildren={<PovHeader avatar="" username="" createdAt="" />}
+        stickyChildren={
+          <BoxedIcon circle>
+            <PlusIcon className="transform rotate-45" />
+          </BoxedIcon>
+        }
       >
-        <h1>Discover</h1>
+        <HeaderController
+          description="This is my description"
+          owner="The owner"
+          embed={{}}
+          title="Point of view"
+        />
+        <h1>POV</h1>
       </MiddlePanel>
     </MainLayout>
   );
