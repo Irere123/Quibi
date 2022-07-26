cd9e5bffd838adb05491b581aecd277bfdb915ec
irere123 <irere2020@outlook.com>
2022-07-27 00:02:24 +0200

    refactor

--- web/src/ui/Modal.tsx
+++ web/src/ui/Modal.tsx
@@ -15,7 +15,7 @@ const customStyles = {
       bottom: "auto",
       marginRight: "-50%",
       borderRadius: 4,
-      padding: "10px",
+      padding: "20px",
       transform: "translate(-50%, -50%)",
       backgroundColor: "var(--color-primary-800)",
       border: "none",
