e89850be43d9dd965711fd592f81662609496f8e
irere123 <irere2020@outlook.com>
2022-07-27 10:16:11 +0200

    refactor(api): added the phoenix pubsub

--- api/lib/okra.ex
+++ api/lib/okra.ex
@@ -1,14 +1,44 @@
 defmodule Okra do
   use Application
 
-  @impl true
   def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+
     children = [
-      # Starts a worker by calling: Api.Worker.start_link(arg)
-      # {Api.Worker, arg}
+
+      {Beef.Repo, []},
+      {Phoenix.PubSub, name: Onion.PubSub},
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: Broth,
+        options: [
+          port: String.to_integer(System.get_env("PORT") || "4001"),
+          dispatch: dispatch(),
+          protocol_options: [idle_timeout: :infinity]
+        ]
+      )
     ]
 
     opts = [strategy: :one_for_one, name: Okra.Supervisor]
-    Supervisor.start_link(children, opts)
+
+    # TODO: make these into tasks
+
+    case Supervisor.start_link(children, opts) do
+      {:ok, pid} ->
+        {:ok, pid}
+      error ->
+        error
+    end
+  end
+
+  defp dispatch do
+    [
+      {:_,
+       [
+         {"/socket", Broth.SocketHandler, []},
+         {:_, Plug.Cowboy.Handler, {Broth, []}}
+       ]}
+    ]
   end
 end
