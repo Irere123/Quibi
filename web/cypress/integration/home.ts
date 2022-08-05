16943a533d1d4e11af386fbf0a3d7f7f27d7b942
irere123 <irere2020@outlook.com>
2022-08-05 11:31:26 +0200

    feat(web): added cypress tests

--- web/cypress/integration/home.ts
+++ web/cypress/integration/home.ts
@@ -1,11 +1,11 @@
 /// <reference  types="cypress" />
 
-// describe("mywebsite", () => {
-//   beforeEach(() => {
-//     cy.visit("http://localhost:3000/dash");
-//   });
+describe("mywebsite", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/dash");
+  });
 
-//   it("has search", () => {
-//     cy.get("input");
-//   });
-// });
+  it("has search", () => {
+    cy.get("input");
+  });
+});
