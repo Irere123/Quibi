c5bd016035dbede36ac1e768ac89051ee2a1d54e
irere123 <irere2020@outlook.com>
2022-08-06 21:14:08 +0200

    new version

--- web/src/modules/academic/events/EventsPage.tsx
+++ web/src/modules/academic/events/EventsPage.tsx
@@ -2,21 +2,21 @@ import React from "react";
 import { Calendar } from "../../../icons";
 import { AccountPageHeader } from "../../../ui/AccountPageHeader";
 import { HeaderController } from "../../display/HeaderController";
-import { AccountLayout } from "../../layouts/AccountLayout";
-import { AccountMainPanel } from "../../layouts/GridPanels";
-import { LeftPanel } from "../LeftPanel";
+import { MiddlePanel } from "../../layouts/GridPanels";
+import { MainLayout } from "../../layouts/MainLayout";
+import { LeftPanel } from "../../layouts/Panels";
 
 export const EventsPage: React.FC = () => {
   return (
-    <AccountLayout leftPanel={<LeftPanel />}>
+    <MainLayout leftPanel={<LeftPanel />}>
       <HeaderController title="Events" />
-      <AccountMainPanel
+      <MiddlePanel
         stickyChildren={
           <AccountPageHeader icon={<Calendar />} title={"Events"} />
         }
       >
         <p>Hello world</p>
-      </AccountMainPanel>
-    </AccountLayout>
+      </MiddlePanel>
+    </MainLayout>
   );
 };
