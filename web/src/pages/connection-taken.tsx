7baf237d9e4e61c9139bf62df4606722bf418714
irere123 <irere2020@outlook.com>
2022-08-21 15:59:19 +0200

    chore(web): added material ui datepickers library

--- web/src/pages/connection-taken.tsx
+++ web/src/pages/connection-taken.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useTypeSafeTranslation } from "../hooks/useTypeSafeTranslation";
+import { HeaderController } from "../modules/display/HeaderController";
 import { Button } from "../ui/Button";
 
 interface ConnectionTakenProps {}
 
-const ConnectionTaken: React.FC<ConnectionTakenProps> = ({}) => {
+const ConnectionTaken: React.FC<ConnectionTakenProps> = () => {
+  const { t } = useTypeSafeTranslation();
   return (
     <div className="flex w-full h-full flex-col items-center justify-center p-8">
+      <HeaderController title={t("header.connectionTaken")} />
       <h4 className="text-primary-100 mb-4">
         Your connection was taken by an other client. Check if it is an browser
         tab
