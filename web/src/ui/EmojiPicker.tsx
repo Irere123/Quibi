871963f709a1c972e248ed5bd164783da96fe9df
irere123 <irere2020@outlook.com>
2022-08-26 16:22:45 +0200

    fix(web): removed tailwind repeated attributtes

--- web/src/ui/EmojiPicker.tsx
+++ web/src/ui/EmojiPicker.tsx
@@ -65,10 +65,10 @@ export const EmojiPicker: React.FC<EmojiPickerProps> = ({
 
   return (
     <div
-      className={`flex bg-primary-700 rounded-8 flex flex-row flex-grow p-1 max-h-24 pt-2 px-2`}
+      className={`flex bg-primary-700 rounded-8 flex-row flex-grow p-1 max-h-24 pt-2 px-2`}
     >
       <div
-        className={`flex grid grid-cols-7 w-full pr-3 gap-2 max-h-16 overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-xl scrollbar-thumb-primary-700`}
+        className={`grid  grid-cols-7 w-full pr-3 gap-2 max-h-16 overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-xl scrollbar-thumb-primary-700`}
       >
         {(queryMatches.length ? queryMatches : emojiSet).map((emoji) => (
           <img
